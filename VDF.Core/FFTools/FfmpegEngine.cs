ac65eaf448b2fda9180e0bb0b9b36f73e4e19162
marcmy <21000174+marcmy@users.noreply.github.com>
2026-09-26 01:49:00 -0400

    fix: restore rotation-safe gray scaling parity

--- VDF.Core/FFTools/FfmpegEngine.cs
+++ VDF.Core/FFTools/FfmpegEngine.cs
@@ -320,7 +320,7 @@ namespace VDF.Core.FFTools {
 
 			if (converter == null || sourceSize != converterSourceSize || srcPixFmt != converterSourcePixelFormat) {
 				converter?.Dispose();
-				converter = new VideoFrameConverter(sourceSize, srcPixFmt, new Size(sideLength, sideLength), AVPixelFormat.AV_PIX_FMT_GRAY8, VideoFrameConverter.ScaleQuality.FastBilinear, false);
+				converter = new VideoFrameConverter(sourceSize, srcPixFmt, new Size(sideLength, sideLength), AVPixelFormat.AV_PIX_FMT_GRAY8, VideoFrameConverter.ScaleQuality.Bicubic, false);
 				converterSourceSize = sourceSize;
 				converterSourcePixelFormat = srcPixFmt;
 			}
@@ -935,7 +935,7 @@ namespace VDF.Core.FFTools {
 					AVPixelFormat destinationPixelFrmt = isGrayByte ? AVPixelFormat.AV_PIX_FMT_GRAY8 : AVPixelFormat.AV_PIX_FMT_YUVJ420P;
 
 					phaseSw.Restart();
-					using var vfc = new VideoFrameConverter(sourceSize, srcPixFmt, destinationSize, destinationPixelFrmt, isGrayByte ? VideoFrameConverter.ScaleQuality.FastBilinear : VideoFrameConverter.ScaleQuality.Bicubic, false);
+					using var vfc = new VideoFrameConverter(sourceSize, srcPixFmt, destinationSize, destinationPixelFrmt, isGrayByte ? VideoFrameConverter.ScaleQuality.Bicubic : VideoFrameConverter.ScaleQuality.Bicubic, false);
 					AVFrame convertedFrame = vfc.Convert(srcFrame);
 					convertMs = phaseSw.ElapsedMilliseconds;
 
